644144abe0b7b2e8cee38a8faf30c1c401eaa70e
Yogesh Singh <ys038573@gmail.com>
2021-10-13 21:43:08 +0530

    Done Basic Changes in font and function calling.

--- wikipedia-search-app.py
+++ wikipedia-search-app.py
@@ -1,20 +1,22 @@
-# github : yogeshsinghgit
-# ->Make Sure to FOllow me on Github <-
+# Github : yogeshsinghgit 
+# ->Make Sure to Follow<-
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 import wikipedia as wiki
 import threading
  
 # function ...
- 
 def search():
     search_data = ent.get()
-    data = wiki.summary(search_data, sentences=8)
+    # insert data
+    search_lbl['text'] = "Searching result for: {}".format(search_data.capitalize())
+    try:
+        data = wiki.summary(search_data, sentences=8)
+    except Exception as e:
+        data = e
     # delete current data ..
     ent.set('')
     text.delete(0.0,END)
-    # insert data
-    search_lbl['text'] = "Searching result for :{}".format(search_data)
     text.insert(0.0,data)
  
 def call_search(*args):
@@ -26,22 +28,24 @@ def call_search(*args):
 root = Tk()
 root.title('Search Application')
 root.geometry('320x480')
+root.resizable(0,0)
 root.configure(bg='white')
  
 # test variable
 ent = StringVar()
-search_entry = Entry(root,width=30,font=('arial',12),bd=2,relief=RIDGE,textvariable=ent)
-search_entry.bind('<Return>',search)
+search_entry = Entry(root,width=30,font=("Sitka Small",12),bd=2,relief=RIDGE,textvariable=ent,justify=CENTER)
+# Bind Entry Box with Enter Key
+search_entry.bind('<Return>',call_search)
 search_entry.place(x=15,y=20)
  
-search_lbl = Label(root,text='Searching result for :  ',font=('arial',12,'bold'),bg='white')
+search_lbl = Label(root,text='Searching result for :',font=('arial',12,'bold'),bg='white')
 search_lbl.place(x=15,y=70)
  
 text = ScrolledText(root,font=('times',12),bd=4,relief=SUNKEN,wrap =WORD)
  
 text.place(x=15,y=100,height=300,width=300)
  
-search_btn = Button(root,text='Search',font=('arial',12,'bold'),width=10,command=search)
+search_btn = Button(root,text='Search',font=('arial',12,'bold'),width=10,command=call_search)
 search_btn.place(x=10,y=420)
  
 clear_btn = Button(root,text='Clear',font=('arial',12,'bold'),width=10,
